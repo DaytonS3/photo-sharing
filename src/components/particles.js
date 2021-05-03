f52acf36c7aaed3bf30db8dcb37611d5213e8908
test kk <00@oo>
2021-05-03 19:12:15 -0400

    link

--- src/components/particles.js
+++ src/components/particles.js
@@ -161,7 +161,7 @@ export class Par extends react.Component {
             fontSize: "24px",
             color: "white"
         }}
-        href="https://dazzling-yalow-c3167b.netlify.app/services">Services
+        href="http://www.socialartdigital.com//services">Services
         </a>
         <a style={{
             fontFamily: 'Commissioner',
